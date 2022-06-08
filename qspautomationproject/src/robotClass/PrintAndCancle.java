16b0ccf69cbdfad199450b3ed052e5505fb7ba4f
gdpoul <107097918+gdpoul@users.noreply.github.com>
2022-06-08 19:25:14 +0530

    Update PrintAndCancle.java

--- qspautomationproject/src/robotClass/PrintAndCancle.java
+++ qspautomationproject/src/robotClass/PrintAndCancle.java
@@ -9,12 +9,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PrintAndCancle {
+	
 	public static void main(String[] args) throws AWTException, InterruptedException {
+		
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://selenium.dev");
-		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(40));
 
 		Thread.sleep(4000);
 		Robot r = new Robot();
