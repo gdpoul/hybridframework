48943f913c20d284af2938c244ec70ff11dbc684
Shree <Shree@DESKTOP-148C48N>
2022-06-08 17:16:59 +0530

    abc

--- qspautomationproject/src/assertionTestNG/OrangeHrmLogin.java
+++ qspautomationproject/src/assertionTestNG/OrangeHrmLogin.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.Reporter;
-import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -31,7 +31,7 @@ public class OrangeHrmLogin {
         driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
         driver.get("https://opensource-demo.orangehrmlive.com/");
 	}
-	@AfterMethod
+	@AfterClass
 	public void tearDown()
 	{
 		driver.quit();
--- qspautomationproject/src/qsp/LaunchChrome1.java
+++ qspautomationproject/src/qsp/LaunchChrome1.java
@@ -1,13 +1,13 @@
 package qsp;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 public class LaunchChrome1 {
 
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		
-		ChromeDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		
 		driver.get("https://www.instagram.com");
 		
