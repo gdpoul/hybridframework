c6f73acc95d919ae56e01a68b3e9ffe7c953dc3c
Shree <Shree@DESKTOP-148C48N>
2022-06-08 19:19:48 +0530

    abc

--- qspautomationproject/src/Practice/Class2.java
+++ qspautomationproject/src/Practice/Class2.java
@@ -8,14 +8,14 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class Class1 {
+public class Class2 {
 
 	public static void main(String[] args) {
 		WebDriver driver=new ChromeDriver();
 		
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(40));
-	WebElement element = driver.findElement(By.xpath(""));	
+	    WebElement element = driver.findElement(By.xpath(""));	
 	
 		
 
