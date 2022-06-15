b061fb44f229675449628decb1054e0460cd0a66
gdpoul <107097918+gdpoul@users.noreply.github.com>
2022-06-15 09:49:55 +0530

    Update LaunchChrome1.java

--- qspautomationproject/src/qsp/LaunchChrome1.java
+++ qspautomationproject/src/qsp/LaunchChrome1.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LaunchChrome1 {
 
 	public static void main(String[] args) throws InterruptedException {
+		
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		
 		WebDriver driver = new ChromeDriver();
@@ -15,7 +16,9 @@ public class LaunchChrome1 {
 		
 		driver.close();
 		
-		//dwqfgrw
+		// abcd
+		
+		
 
 	}
 
